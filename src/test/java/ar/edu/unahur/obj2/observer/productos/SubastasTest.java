037a5f3abb46d48e5b7937bc556fd4735cc4dc0b
NicolasUrdiales <nicolas.urdiales@estudiantes.unahur.edu.ar>
2025-11-03 10:36:30 -0300

    tests realizados(falta bonus)

--- src/test/java/ar/edu/unahur/obj2/observer/productos/SubastasTest.java
+++ src/test/java/ar/edu/unahur/obj2/observer/productos/SubastasTest.java
@@ -1,9 +1,12 @@
 package ar.edu.unahur.obj2.observer.productos;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 
+import ar.edu.unahur.obj2.observer.excepciones.ErrorSubastador;
 import ar.edu.unahur.obj2.observer.subastadores.Subastador;
 
 public class SubastasTest {
@@ -29,11 +32,46 @@ public class SubastasTest {
 
 
     @Test
+    @DisplayName("Dado el escenario 1, se verifica que los subastadores gonzager y martomau reciben correctamente la última oferta realizada")
     void test1(){
         setUp();
         assertEquals(s1.getUltimaOferta(), p1.getOfertas().getLast());
         assertEquals(s2.getUltimaOferta(), p1.getOfertas().getLast());
     }
+
+    @Test
+    void test2(){
+        setUp();
+        assertEquals("martomau", p1.getOfertas().getLast().getNombreSubastador());
+    }
+
+
+    @Test
+    void test3(){
+        setUp();
+        assertEquals(Integer.valueOf(30) , s1.getUltimaOferta().getValor());
+    }
+
+    @Test
+    void test4(){
+        setUp();
+        assertEquals(3, p1.getOfertas().size());
+    }
+
+    @Test
+    void test5(){
+        setUp();
+        Subastador s3 = new Subastador("diazdan");
+
+        String mensajeEsperado = "El subastador no participa en la subasta";
+
+        RuntimeException exception = assertThrows(ErrorSubastador.class, () -> {
+            s3.realizarOferta(p1);
+        });
+
+        assertEquals(mensajeEsperado, exception.getMessage());
+    }
+
 }
 
 
